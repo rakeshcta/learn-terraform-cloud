30e4dd4f8dc05e55bab76ff22fdb997911a45b65
T <rakesht@deloitte.com>
2022-08-02 13:14:37 +0530

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-
+/*
   cloud {
     organization = "hashedin_rakesh"
 
@@ -7,7 +7,7 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
